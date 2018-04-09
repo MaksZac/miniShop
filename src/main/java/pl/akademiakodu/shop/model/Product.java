0dc77fe0ddb4bd78e2c3a08029242178381a8cf2
Maks <maks.zaczkiewicz@gmail.com>
2018-04-09 17:38:04 +0200

    initial commit

---
+++ src/main/java/pl/akademiakodu/shop/model/Product.java
@@ -0,0 +1,30 @@
+package pl.akademiakodu.shop.model;
+
+
+public class Product {
+
+    private String name;
+
+    private String description;
+
+    public Product(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
